b23a88ae0a6c2c0497ad2789909e81fa9b9b926a
Graeme Gellatly <gdgellatly@gmail.com>
2019-11-06 21:47:46 +1300

    Better Multicompany for account_financial_risk

--- account_financial_risk/models/__init__.py
+++ account_financial_risk/models/__init__.py
@@ -1,4 +1,3 @@
 from . import account_invoice
-from . import res_company
 from . import res_config
 from . import res_partner
--- account_financial_risk/models/res_config.py
+++ account_financial_risk/models/res_config.py
@@ -5,7 +5,27 @@ from odoo import fields, models
 
 
 class AccountConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     invoice_unpaid_margin = fields.Integer(
-        related='company_id.invoice_unpaid_margin', readonly=False)
+        config_param="account_financial_risk.invoice_unpaid_margin", readonly=False
+    )
+
+    def set_values(self):
+        params = self.env["ir.config_parameter"].sudo()
+        before_margin = int(
+            params.get_param("account_financial_risk.invoice_unpaid_margin")
+        )
+        super().set_values()
+        after_margin = self.invoice_unpaid_margin
+        if before_margin != after_margin:
+            self.env.cr.execute(
+                "SELECT DISTINCT partner_id FROM account_move_line "
+                "WHERE partner_id IS NOT NULL"
+            )
+            partners = (
+                self.env["res.partner"]
+                .sudo()
+                .browse([r[0] for r in self.env.cr.fetchall()])
+            )
+            partners._compute_risk_account_amount()
